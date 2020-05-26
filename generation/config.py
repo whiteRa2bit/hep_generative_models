3523db576e67420075e06e22a04cf1aa0fa2e939
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-05-26 14:41:05 +0300

    Add PROCESSING_TIME_NORM_COEF constant

--- generation/config.py
+++ generation/config.py
@@ -3,3 +3,6 @@ import os
 PROJECT_DIR = '/home/pavel/MyDocs/work/lambda/generation/'
 DATA_DIR = os.path.join(PROJECT_DIR, 'source_data/')
 CHECKPOINTS_DIR = os.path.join(PROJECT_DIR, 'checkpoints/')
+
+
+PROCESSING_TIME_NORM_COEF = 5
