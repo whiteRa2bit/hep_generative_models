0db5d63d5a016521b429b9fe5c7b95245c4fc0cc
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-05-26 22:29:07 +0300

    Data preparation (#4)
    * Add doc strings and fix data_preparation
    
    * Add data_folder param
    
    * Add postprocess_output
    
    * Add PROCESSING_TIME_NORM_COEF constant
    
    * Add data_example.ipynb
    
    * Delete eda notebook
    
    * Change default params + add description to doc strings
    
    * Unite events data + add sample_coef param
    
    * Delete get_detector_data func
    
    * Delete get_detector_data func
    
    * Rename param in signature of generate_detector_event_output
    
    * Add generate_single_detector_output and generate_detector_output
    
    * Add get_detector_df
    
    * Add comparison for single and multiple events

--- generation/data/data_preparation.py
+++ generation/data/data_preparation.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import pandas as pd
 import argparse
+from generation.config import PROCESSING_TIME_NORM_COEF
 
 
 def parse_args():
@@ -11,48 +12,51 @@ def parse_args():
     return parser.parse_args()
 
 
-def process_file(data, attr_name, args):
-    with open("{}/{}.txt".format(args.data_folder, attr_name)) as file_:
+def process_file(data, attr_name: str, data_folder: str) -> None:
+    """
+    Adds values of corresponding attribute to data
+    :param data: Dictionary where keys are attributes' names
+    :param attr_name: attribute name
+    :param data_folder: path where to read data
+    :return:
+    """
+    with open("{}/{}.txt".format(data_folder, attr_name)) as file_:
         for line in file_:
             data[attr_name].append(float(line.strip()))
 
-    attrs_values = data.values()
-    data_size = len(attrs_values[0])
-    assert all(len(item) == data_size for item in attrs_values)
 
-
-def get_events_df():
+def get_events_df(data_folder):
+    """
+    :param data_folder:
+    :return: events df, where each column is an attribute
+    """
     attributes = ['x', 'y', 'z', 'energy', 'detector', 'timestamp', 'event']
     data = {attr_name: [] for attr_name in attributes}
 
     for attr_name in attributes:
         print("Attribute: {}".format(attr_name))
         time.sleep(1)
-        process_file(data, attr_name)
+        process_file(data, attr_name, data_folder)
         time.sleep(1)
+
+    # Check that all attributes have
+    # the same number of values
+    attrs_values = data.values()
+    data_size = len(attrs_values[0])
+    assert all(len(item) == data_size for item in attrs_values)
 
     df = pd.DataFrame(data)
     return df
 
 
-def get_detector_event_data(df_full, detector: int, event: int):
-    df = df_full[df_full['detector'] == detector]
-    df = df[df['event'] == event]
-    df.sort_values(by='timestamp', inplace=True)
-
-    return df
-
-
-def generate_detector_event_output(df_full, detector: int, event: int, steps_num: int = 500):
+def generate_single_detector_output(df, steps_num: int = 1024, sample_coef: float = 0.5):
     """
-    Generates one output for given detector and event
-    :param df_full: df with info of given detector and event
-    :param detector: detector number
-    :param event: event number
+    Generates one output for given detector
+    :param df: df with info of given detector and event
     :param steps_num: number of timestamps by which time will be splitted
+    :param sample_coef: percent of data to take for each step
     :return: np.array [steps_num] with energies
     """
-    df = get_detector_event_data(df_full, detector, event)
     min_timestamp = min(df['timestamp'])
     max_timestamp = max(df['timestamp'])
     step = (max_timestamp - min_timestamp) / steps_num
@@ -61,15 +65,71 @@ def generate_detector_event_output(df_full, detector: int, event: int, steps_num
     for i in range(steps_num):
         step_df = df[df['timestamp'] > i * step]
         step_df = step_df[step_df['timestamp'] < (i + 1) * step]
-        step_df = step_df.sample(len(step_df) // 2)  # randomly sample half of data
+        step_df = step_df.sample(int(len(step_df) * sample_coef))  # randomly sample data
         step_energy = sum(step_df['energy'])
         step_energies.append(step_energy)
 
     return np.array(step_energies)
 
 
+def get_detector_df(df_full, detector: int, event: int = -1):
+    if event == -1:
+        df = df_full[df_full['detector'] == detector].sort_values(by=['timestamp'])
+    else:
+        df = df_full[(df_full['detector'] == detector) & (df_full['event'] == event)]
+    df.sort_values(by=['timestamp'], inplace=True)
+
+    return df
+
+
+def generate_detector_output(df_full, data_size: int,  use_postprocessing: bool, detector: int,
+                             event: int = -1, steps_num: int = 1024, sample_coef: float = 0.5):
+    """
+    Generates data for given detector
+    :param df_full: pandas df, output of get_events_df()
+    :param data_size: number of samples to get
+    :param use_postprocessing: whether to use output before or after photodetector
+    :param detector: detector number
+    :param event: event number
+    :param steps_num: number of timestamps by which time will be splitted
+    :param sample_coef: percent of data to take for each step
+    :return: np.array with generated events
+    """
+    df = get_detector_df(df_full, detector, event)
+
+    outputs = []
+    for _ in range(data_size):
+        step_energies = generate_single_detector_output(df, steps_num, sample_coef)
+        if use_postprocessing:
+            output = postprocess_step_energies(step_energies)
+        outputs.append(output)
+
+    return np.array(outputs)
+
+
+def postprocess_step_energies(step_energies):
+    """
+    Getting result signal after photodetector
+    :param step_energies: Output from generate_detector_event_output
+    :return: processed signal
+    """
+    def build_kernel(x_cur, energy, x_min, x_max):
+        kernel = lambda x: ((x - x_cur) ** 2) / np.exp((x - x_cur) / PROCESSING_TIME_NORM_COEF)
+        x_linspace = np.linspace(x_min, x_max, x_max - x_min)
+        y_linspace = energy * np.array(list(map(kernel, x_linspace)))
+        y_linspace[:x_cur] = np.zeros(x_cur)
+        return y_linspace
+
+    result = np.zeros(len(step_energies))
+    for x_cur, energy in enumerate(step_energies):
+        y_cur = build_kernel(x_cur, energy, x_min=0, x_max=len(step_energies))
+        result += y_cur
+    return result
+
+
 def main():
-    df = get_events_df()
+    args = parse_args()
+    df = get_events_df(args.data_folder)
     print(df.head())
 
 
