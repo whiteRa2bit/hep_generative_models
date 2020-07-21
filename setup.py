5adc2c7b8b77b76a5b64b8aeed182386ba8f117a
whiteRa2bit <pavel.fakanov@gmail.com>
2020-07-21 19:23:12 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 requirements = [
-    # "torch==1.4.0",
+    "torch==1.4.0",
     "scipy==1.4.1",
-    "numpy==1.16.2",
-    # "matplotlib==3.2.1",
+    "numpy==1.18.1",
+    "matplotlib==3.2.1",
 ]
 
 setup(
-    name='hgm',
+    name='generation',
     version='0.1',
     description='Module of generative models applied to high energy physics problems',
     license="MIT",
