2182010869a6fae200b87fdbbd6a6e4803d254ab
whiteRa2bit <pavel.fakanov@gmail.com>
2020-07-21 16:53:51 +0300

    Update package name

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 requirements = [
-#    "torch==1.4.0",
+    # "torch==1.4.0",
     "scipy==1.4.1",
     "numpy==1.16.2",
-    "matplotlib==3.2.1",
+    # "matplotlib==3.2.1",
 ]
 
 setup(
-   name='generation',
-   version='0.1',
-   description='Module of generative models applied to high energy physics problems',
-   license="MIT",
-   long_description=long_description,
-   author='Pavel Fakanov',
-   author_email='pavel.fakanov@gmail.com',
-   packages=find_packages(),
-   install_requires=requirements,
+    name='hgm',
+    version='0.1',
+    description='Module of generative models applied to high energy physics problems',
+    license="MIT",
+    long_description=long_description,
+    author='Pavel Fakanov',
+    author_email='pavel.fakanov@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements,
 )
