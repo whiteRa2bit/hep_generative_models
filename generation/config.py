b5868a604c9e2679976f9d92a12ff4ea7a2affcf
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-08-06 21:22:56 +0300

    Data preparation (#12)
    * Update generate_detector_df
    
    * Update get_detector_event_df

--- generation/config.py
+++ generation/config.py
@@ -2,10 +2,12 @@ import os
 
 PROJECT_DIR = '/home/pavel/MyDocs/work/lambda/generation/'
 ROOT_DIR = '/home/pafakanov/data/'
+
 DATA_DIR = os.path.join(ROOT_DIR, 'hep_data/')
 SPACAL_DATA_DIR = os.path.join(DATA_DIR, 'spacal_simulation')
 SPACAL_DATA_PATH = os.path.join(SPACAL_DATA_DIR, 'particles.pkl')
 CHECKPOINTS_DIR = os.path.join(PROJECT_DIR, 'checkpoints/')
-
+ATTRIBUTES = ['x', 'y', 'z', 'energy', 'detector', 'timestamp', 'event']
+ATTRIBUTE_PATHS = [os.path.join(SPACAL_DATA_DIR, f'{attr}.txt') for attr in ATTRIBUTES]
 
 PROCESSING_TIME_NORM_COEF = 5
