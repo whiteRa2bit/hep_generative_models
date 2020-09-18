156de2958e9d147495a3b0ffc4813cec02690d16
whiteRa2bit <pavel.fakanov@gmail.com>
2020-09-18 14:13:29 +0000

    Apply yapf for config

--- generation/config.py
+++ generation/config.py
@@ -1,12 +1,10 @@
 import os
 import collections
 
-
 # Data params
 _ENERGY = 1
 PROJECT_DIR = '/home/pavel/MyDocs/work/lambda/generation/'
 DATA_DIR = f'/home/pafakanov/data/hep_data/spacal_simulation/{_ENERGY}GeV/'
-
 
 CHECKPOINTS_DIR = os.path.join(PROJECT_DIR, 'checkpoints/')
 
@@ -20,12 +18,14 @@ ROOT_FILE_PATH = os.path.join(RAW_DATA_DIR, 'hybrid0.root')
 ROOT_TREE_NAME = 'hybrid'
 
 # TODO: (@whiteRa2bit, 2020-08-25) Replace energy with PhotonEnergy
-ATTRIBUTES = ['x', 'y', 'z', 'energy', 'detector', 'timestamp', 'event']  # TODO: (@whiteRa2bit, 2020-08-25) Add namedtuple
+ATTRIBUTES = ['x', 'y', 'z', 'energy', 'detector', 'timestamp',
+              'event']  # TODO: (@whiteRa2bit, 2020-08-25) Add namedtuple
 INT_ATTRIBUTES = ['event', 'detector']
-ATTRIBUTE_PATHS = [os.path.join(RAW_DATA_DIR, f'{attr}.npy') for attr in ATTRIBUTES]
+ATTRIBUTE_PATHS = [
+    os.path.join(RAW_DATA_DIR, f'{attr}.npy') for attr in ATTRIBUTES
+]
 SPACAL_DATA_PATH = os.path.join(RAW_DATA_DIR, 'particles.pkl')
 
-
 # Processing constans
-STEPS_NUM = 128
+STEPS_NUM = 2048
 PROCESSING_TIME_NORM_COEF = 50
