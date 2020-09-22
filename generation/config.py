9e304177e17d87fdb8268f0dcfb888c362348495
whiteRa2bit <pavel.fakanov@gmail.com>
2020-09-22 13:59:13 +0000

    Update config

--- generation/config.py
+++ generation/config.py
@@ -5,7 +5,6 @@ import collections
 _ENERGY = 1
 DATA_DIR = f'/home/pafakanov/data/hep_data/spacal_simulation/{_ENERGY}GeV/'
 CHECKPOINTS_DIR = os.path.join(DATA_DIR, 'checkpoints/')
-
 DF_DIR = os.path.join(DATA_DIR, 'dfs')
 SIGNAL_DIR = os.path.join(DATA_DIR, 'signals')
 RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw_data')
@@ -14,7 +13,6 @@ TRAINING_DATA_DIR = os.path.join(DATA_DIR, 'training_data')
 # Root params
 ROOT_FILE_PATH = os.path.join(RAW_DATA_DIR, 'hybrid0.root')
 ROOT_TREE_NAME = 'hybrid'
-
 ATTRIBUTES = ['x', 'y', 'z', 'PhotonEnergy', 'detector', 'timestamp',
               'event']  # TODO: (@whiteRa2bit, 2020-08-25) Add namedtuple
 INT_ATTRIBUTES = ['event', 'detector']
@@ -24,3 +22,5 @@ SPACAL_DATA_PATH = os.path.join(RAW_DATA_DIR, 'particles.pkl')
 # Processing params
 STEPS_NUM = 2048
 PROCESSING_TIME_NORM_COEF = 50
+REPEAT_COEF = 100
+FRAC_COEF = 0.7
