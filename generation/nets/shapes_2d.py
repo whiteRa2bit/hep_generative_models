123274de7c8dd2b6e6cdd4d315c2bc05b02e04c7
whiteRa2bit <pavel.fakanov@gmail.com>
2020-09-23 21:14:25 +0000

    Add batchnorm + deeper net

--- generation/nets/shapes_2d.py
+++ generation/nets/shapes_2d.py
@@ -8,13 +8,18 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.z_dim = config["z_dim"]
         
-        self.fc1 = nn.Linear(self.z_dim, 64)
-        self.fc2 = nn.Linear(64, 784)
+        self.fc1 = nn.Linear(self.z_dim, 400)
         
+        self.batchnorm1 = nn.BatchNorm2d(16)
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        self.batchnorm3 = nn.BatchNorm2d(16)
+        self.batchnorm4 = nn.BatchNorm2d(8)
+
         self.conv1 = nn.ConvTranspose2d(4, 16, 3, stride=2)
-        self.conv2 = nn.ConvTranspose2d(16, 32, 3, stride=1)
-        self.conv3 = nn.Conv2d(32, 16, 3)
-        self.conv4 = nn.Conv2d(16, 4, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.conv3 = nn.ConvTranspose2d(32, 16, 3, stride=2)
+        self.conv4 = nn.ConvTranspose2d(16, 8, 3, stride=2)
+        self.conv5 = nn.Conv2d(8, 4, 3)
         
     def forward(self, x, debug=False):
         def _debug():
@@ -24,18 +29,22 @@ class Generator(nn.Module):
         _debug()
         x = F.leaky_relu(self.fc1(x))
         _debug()
-        x = F.leaky_relu(self.fc2(x))
-        _debug()
 
-        x = x.view(-1, 4, 14, 14)
+        x = x.view(-1, 4, 10, 10)
         _debug()
         x = F.leaky_relu(self.conv1(x))
+        x = self.batchnorm1(x)
         _debug()
         x = F.leaky_relu(self.conv2(x))
+        x = self.batchnorm2(x)
         _debug()
         x = F.leaky_relu(self.conv3(x))
+        x = self.batchnorm3(x)
         _debug()
         x = F.leaky_relu(self.conv4(x))
+        x = self.batchnorm4(x)
+        _debug()
+        x = F.leaky_relu(self.conv5(x))
         _debug()
         return x
 
@@ -43,13 +52,16 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, config):
         super(Discriminator, self).__init__()
-        self.pool = nn.MaxPool2d(5, 3)
+        self.pool = nn.AvgPool2d(5, 3)
         self.conv1 = nn.Conv2d(4, 16, 5, padding=2)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.conv3 = nn.Conv2d(32, 16, 3, padding=1)
 
-        self.fc1 = nn.Linear(16 * 2 * 2, 32)
-        self.fc2 = nn.Linear(32, 1)
+        self.batchnorm1 = nn.BatchNorm2d(16)
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        self.batchnorm3 = nn.BatchNorm2d(16)
+
+        self.fc1 = nn.Linear(16 * 7 * 7, 1)
 
     def forward(self, x, debug=False):
         def _debug():
@@ -58,20 +70,21 @@ class Discriminator(nn.Module):
 
         _debug()
         x = F.leaky_relu(self.conv1(x))
+        x = self.batchnorm1(x)
         _debug()
         x = self.pool(x)
         _debug()
         x = F.leaky_relu(self.conv2(x))
+        x = self.batchnorm2(x)
         _debug()
         x = self.pool(x)
         _debug()
         x = F.leaky_relu(self.conv3(x))
+        x = self.batchnorm3(x)
         _debug()
 
-        x = x.view(-1, 16 * 2 * 2)
+        x = x.view(-1, 16 * 7 * 7)
         _debug()
-        x = F.leaky_relu(self.fc1(x))
-        _debug()
-        x = F.leaky_relu(self.fc2(x))
+        x = self.fc1(x)
         _debug()
         return x
