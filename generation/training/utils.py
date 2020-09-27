1e6eb17eacf406b7bb146a7c2c91d9db179a539c
whiteRa2bit <pavel.fakanov@gmail.com>
2020-09-27 08:31:54 +0000

    Add seed logging

--- generation/training/utils.py
+++ generation/training/utils.py
@@ -5,6 +5,7 @@ from generation.config import RANDOM_SEED
 
 
 def set_seed(seed=RANDOM_SEED):
+    print(f"Set seed {seed}")
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
