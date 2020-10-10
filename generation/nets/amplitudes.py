f56dec9e71e5c0da74daa6ea10b23d82a6db33ea
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-10-10 20:59:57 +0300

    Amplitudes (#37)
    * Update default config
    
    * Update deps
    
    * Fix wandb logging
    
    * Update generator/discriminator training
    
    * Fix wandb logging + layernorm for wgan-gp
    
    * Add seed logging
    
    * Delete unused notebooks
    
    * Update config
    
    * Bump version
    
    * Fix wandb logging bug
    
    * update notebook
    
    * Add presentation folder
    
    * Fix yapf
    
    * Add amplitudes dataset
    
    * Add amplitudes net
    
    * Add amplitudes notebook
    
    * Delete unused notebooks
    
    * Bump version
    
    * Add amplitudes config
    
    * Update amplitdues net
    
    * Update notebooks
    
    * Add train script for amplitudes

---
+++ generation/nets/amplitudes.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import wandb
+
+
+class Generator(nn.Module):
+    def __init__(self, config):
+        super(Generator, self).__init__()
+        self.x_dim = config['x_dim']
+        self.z_dim = config['z_dim']
+
+        self.fc1 = nn.Linear(self.z_dim, (self.x_dim + self.z_dim) // 2)
+        self.fc2 = nn.Linear((self.x_dim + self.z_dim) // 2, self.x_dim)
+        self.fc3 = nn.Linear(self.x_dim, self.x_dim)
+
+    def forward(self, x, debug=False):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return torch.clamp(x, 0, 1)
+
+    @staticmethod
+    def visualize(generated, real, epoch):
+        generated_sample = generated[0].cpu().data
+        real_sample = real[0].cpu().data
+
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+        ax[0].set_title("Generated")
+        ax[0].plot(generated_sample)
+        ax[1].set_title("Real")
+        ax[1].plot(real_sample)
+        wandb.log({"generated_real": fig})
+        plt.clf()
+
+
+class Discriminator(nn.Module):
+    def __init__(self, config):
+        super(Discriminator, self).__init__()
+        self.x_dim = config['x_dim']
+        self.z_dim = config['z_dim']
+
+        self.fc1 = nn.Linear(self.x_dim, self.x_dim)
+        self.fc2 = nn.Linear(self.x_dim, (self.x_dim + self.z_dim) // 2)
+        self.fc3 = nn.Linear((self.x_dim + self.z_dim) // 2, 1)
+
+    def forward(self, x, debug=False):
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
