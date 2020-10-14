5f06f0764700a57eae54a828a727af346f8be050
whiteRa2bit <pavel.fakanov@gmail.com>
2020-10-14 16:07:11 +0000

    Add generate_one_signal import

--- tools/data_preparation/prepare_signals.py
+++ tools/data_preparation/prepare_signals.py
@@ -6,8 +6,8 @@ import numpy as np
 import tqdm
 import h5py
 
-from generation.config import FULL_SIGNAL_DIR, TRUNCATED_SIGNALS_DIR, REPEAT_COEF, FRAC_COEF, H5_DATASET_NAME
-from generation.dataset.data_utils import save_h5, get_attributes_df, get_event_detector_df, generate_signals, get_detector_training_data_path, create_dir
+from generation.config import FULL_SIGNALS_DIR, TRUNCATED_SIGNALS_DIR, REPEAT_COEF, FRAC_COEF, H5_DATASET_NAME
+from generation.dataset.data_utils import save_h5, get_attributes_df, get_event_detector_df, generate_one_signal, generate_signals, get_detector_training_data_path, create_dir
 
 _PROCESSORS_NUM = 16
 
@@ -25,7 +25,7 @@ def _process_event_detector(event, detector, n_signals=REPEAT_COEF, frac_coef=FR
 
 
 def _save_detector_signals(full_signals, trunc_signals, detector):
-    full_data_path = get_detector_data_path(detector)
+    full_data_path = get_detector_signals_path(detector)
     trunc_data_path = get_detector_training_data_path(detector)
     save_h5(full_signals, H5_DATASET_NAME, full_data_path)
     save_h5(trunc_signals, H5_DATASET_NAME, trunc_data_path)
