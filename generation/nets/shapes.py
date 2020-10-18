1a6a7164cfc9fe4454e4f1035bcb32345391f4a1
whiteRa2bit <pavel.fakanov@gmail.com>
2020-10-18 14:18:30 +0000

    Add signals net + renames shapes net

--- generation/nets/shapes.py
+++ generation/nets/shapes.py
@@ -15,14 +15,14 @@ class Generator(nn.Module):
 
         self.conv1 = nn.Conv1d(1, 8, 3, padding=1)
         self.conv2 = nn.Conv1d(8, 32, 3, padding=1)
-        self.conv3 = nn.Conv1d(32, 64, 3, padding=1)
-        self.conv4 = nn.Conv1d(64, 16, 3, padding=1)
-        self.conv5 = nn.Conv1d(16, 9, 3, padding=1)
+        self.conv3 = nn.Conv1d(32, 16, 3, padding=1)
+        self.conv4 = nn.Conv1d(16, 8, 3, padding=1)
+        self.conv5 = nn.Conv1d(8, 1, 3, padding=1)
 
         self.batchnorm1 = nn.BatchNorm1d(8)
         self.batchnorm2 = nn.BatchNorm1d(32)
-        self.batchnorm3 = nn.BatchNorm1d(64)
-        self.batchnorm4 = nn.BatchNorm1d(16)
+        self.batchnorm3 = nn.BatchNorm1d(16)
+        self.batchnorm4 = nn.BatchNorm1d(8)
 
     def forward(self, x, debug=False):
         def _debug():
@@ -48,17 +48,19 @@ class Generator(nn.Module):
         x = self.conv5(x)
         _debug()
 
-        return x
+        return torch.sigmoid(x.squeeze(1))
 
     @staticmethod
     def visualize(generated, real, epoch):
         generated_sample = generated[0].cpu().data
         real_sample = real[0].cpu().data
 
-        fig, ax = plt.subplots(3, 3, figsize=(10, 10))
-        for i in range(9):
-            ax[i // 3][i % 3].plot(generated_sample[i])
-        wandb.log({"Generated": fig})
+        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+        ax[0].set_title("Generated")
+        ax[0].plot(generated_sample)
+        ax[1].set_title("Real")
+        ax[1].plot(real_sample)
+        wandb.log({"generated_real": fig})
         plt.clf()
 
 
@@ -70,11 +72,11 @@ class Discriminator(nn.Module):
         self.fc_final = nn.Linear(288, 1)
 
         self.pool = nn.AvgPool1d(5, 3)
-        self.conv1 = nn.Conv1d(9, 16, 7, padding=3)
-        self.conv2 = nn.Conv1d(16, 32, 3, padding=1)
+        self.conv1 = nn.Conv1d(1, 8, 7, padding=3)
+        self.conv2 = nn.Conv1d(8, 32, 3, padding=1)
         self.conv3 = nn.Conv1d(32, 8, 3, padding=1)
 
-        self.layernorm1 = nn.LayerNorm([16, 1024])
+        self.layernorm1 = nn.LayerNorm([8, 1024])
         self.layernorm2 = nn.LayerNorm([32, 340])
         self.layernorm3 = nn.LayerNorm([8, 112])
 
@@ -83,8 +85,9 @@ class Discriminator(nn.Module):
             if debug:
                 print(x.shape)
 
-        x = self.conv1(x)
+        x = x.unsqueeze(1)
         _debug()
+        x = self.conv1(x)
         x = F.leaky_relu(self.layernorm1(x))
         _debug()
         x = self.pool(x)
