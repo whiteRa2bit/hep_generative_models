13b8b50403b98e2e5caf974748a9799ad41d7e71
whiteRa2bit <pavel.fakanov@gmail.com>
2020-10-18 14:23:53 +0000

    Fix signals training imports

--- tools/train/train_signals.py
+++ tools/train/train_signals.py
@@ -2,7 +2,7 @@ import torch
 
 from generation.config import SIGNALS_TRAINING_CONFIG as CONFIG
 from generation.dataset.signals_dataset import SignalsDataset
-from generation.nets.shapes import Generator, Discriminator
+from generation.nets.signals import Generator, Discriminator
 from generation.training.wgan_trainer import WganTrainer
 from generation.training.utils import set_seed
 
