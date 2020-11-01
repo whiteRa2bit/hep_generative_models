9cf79bfbd59bb8337d9c552742df80eb877a9684
whiteRa2bit <pavel.fakanov@gmail.com>
2020-11-01 10:29:39 +0000

    Add separate lr for gen and dis

--- tools/train/train_signals.py
+++ tools/train/train_signals.py
@@ -12,8 +12,8 @@ def run_train(config=CONFIG):
 
     generator = Generator(config)
     discriminator = Discriminator(config)
-    g_optimizer = torch.optim.Adam(generator.parameters(), lr=config['lr'], betas=(0, 0.9))
-    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=config['lr'], betas=(0, 0.9))
+    g_optimizer = torch.optim.Adam(generator.parameters(), lr=config['g_lr'])
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=config['d_lr'])
 
     trainer = WganTrainer(generator, discriminator, g_optimizer, d_optimizer, config)
     trainer.run_train(dataset)
