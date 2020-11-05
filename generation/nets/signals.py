d57a8407d654d457935e954a026ddaa578a90684
whiteRa2bit <pavel.fakanov@gmail.com>
2020-11-05 21:07:50 +0000

    decrease d_coef

--- generation/nets/signals.py
+++ generation/nets/signals.py
@@ -64,47 +64,34 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.x_dim = config['x_dim']
 
-        self.pool = nn.AvgPool1d(5, 3)
-        self.conv1 = nn.Conv1d(9, 16, 7, padding=3)
-        self.conv2 = nn.Conv1d(16, 8, 5, padding=2)
-        # self.conv3 = nn.Conv1d(32, 8, 5, padding=2)
+        # self.pool = nn.AvgPool1d(5, 3)
+        # self.conv1 = nn.Conv1d(9, 16, 7, padding=3)
+        # self.conv2 = nn.Conv1d(16, 8, 5, padding=2)
+        # # self.conv3 = nn.Conv1d(32, 8, 5, padding=2)
 
-        layernorm_dim = config["x_dim"]
-        self.layernorm1 = nn.LayerNorm([16, layernorm_dim])
-        layernorm_dim = (layernorm_dim - 2) // 3
-        self.layernorm2 = nn.LayerNorm([8, layernorm_dim])
-        layernorm_dim = (layernorm_dim - 2) // 3
+        # layernorm_dim = config["x_dim"]
+        # self.layernorm1 = nn.LayerNorm([16, layernorm_dim])
+        # layernorm_dim = (layernorm_dim - 2) // 3
+        # self.layernorm2 = nn.LayerNorm([8, layernorm_dim])
+        # layernorm_dim = (layernorm_dim - 2) // 3
         # self.layernorm3 = nn.LayerNorm([8, layernorm_dim])
         # layernorm_dim = (layernorm_dim - 2) // 3
 
-        self.fc_final = nn.Linear(8 * layernorm_dim, 1)
+        self.fc1 = nn.Linear(self.x_dim, 64)
+        self.fc2 = nn.Linear(64, 8)
+        self.fc_final = nn.Linear(8 * 9, 1)
 
     def forward(self, x, debug=False):
         def _debug():
             if debug:
                 print(x.shape)
 
-        x = self.conv1(x)
+        x = torch.tanh(self.fc1(x))
         _debug()
-        x = F.leaky_relu(self.layernorm1(x))
+        x = torch.tanh(self.fc2(x))
         _debug()
-        x = self.pool(x)
-        _debug()
-        x = self.conv2(x)
-        x = F.leaky_relu(self.layernorm2(x))
-        _debug()
-        x = self.pool(x)
-        _debug()
-        # x = self.conv3(x)
-        # x = F.leaky_relu(self.layernorm3(x))
-        # _debug()
-        # x = self.pool(x)
-        # _debug()
         x = x.view(x.shape[0], -1)
-
-        x = x.squeeze(1)
         _debug()
         x = self.fc_final(x)
-        _debug()
 
         return x
