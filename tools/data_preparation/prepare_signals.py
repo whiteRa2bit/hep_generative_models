5789b6ddc5dc790b67e4c5abcb100ad02ec24c28
Pavel Fakanov <pavel.fakanov@gmail.com>
2020-11-13 20:42:54 +0300

    Multiple root files (#43)
    * Bump version
    
    * Rename trunc->frac + add event_attr + add root_files_dir
    
    * trunc -> frac
    
    * Support multiple root files
    
    * trunc -> frac
    
    * change data dir
    
    * fix typo

--- tools/data_preparation/prepare_signals.py
+++ tools/data_preparation/prepare_signals.py
@@ -7,33 +7,33 @@ import tqdm
 import h5py
 from loguru import logger
 
-from generation.config import FULL_SIGNALS_DIR, TRUNCATED_SIGNALS_DIR, REPEAT_COEF, FRAC_COEF, H5_DATASET_NAME
+from generation.config import FULL_SIGNALS_DIR, FRAC_SIGNALS_DIR, REPEAT_COEF, FRAC_COEF, H5_DATASET_NAME
 from generation.dataset.data_utils import save_h5, get_attributes_df, get_event_detector_df, generate_one_signal, \
     generate_signals, get_detector_signals_path, get_detector_training_data_path, create_dir
 
 _PROCESSORS_NUM = 16
 
 
-def _create_dirs(full_signals_dir: str = FULL_SIGNALS_DIR, trunc_signals_dir: str = TRUNCATED_SIGNALS_DIR):
+def _create_dirs(full_signals_dir: str = FULL_SIGNALS_DIR, frac_signals_dir: str = FRAC_SIGNALS_DIR):
     create_dir(full_signals_dir)
-    create_dir(trunc_signals_dir)
+    create_dir(frac_signals_dir)
 
 
 def _process_event_detector(event, detector, n_signals=REPEAT_COEF, frac_coef=FRAC_COEF):
     event_detector_df = get_event_detector_df(event, detector)
     full_signal = generate_one_signal(event_detector_df)
-    trunc_signals = generate_signals(event_detector_df, n_signals, frac=frac_coef)
-    return full_signal, trunc_signals
+    frac_signals = generate_signals(event_detector_df, n_signals, frac=frac_coef)
+    return full_signal, frac_signals
 
 
-def _save_detector_signals(full_signals, trunc_signals, detector):
+def _save_detector_signals(full_signals, frac_signals, detector):
     full_data_path = get_detector_signals_path(detector)
-    trunc_data_path = get_detector_training_data_path(detector)
+    frac_data_path = get_detector_training_data_path(detector)
     save_h5(full_signals, H5_DATASET_NAME, full_data_path)
-    save_h5(trunc_signals, H5_DATASET_NAME, trunc_data_path)
+    save_h5(frac_signals, H5_DATASET_NAME, frac_data_path)
 
 
-def main():  # TODO: (@whiteRa2bit, 2020-10-14) Create training_data folder
+def main():
     _create_dirs()
     df_full = get_attributes_df()
     events = sorted(df_full['event'].unique())
@@ -46,13 +46,13 @@ def main():  # TODO: (@whiteRa2bit, 2020-10-14) Create training_data folder
             events_signals = list(tqdm.tqdm(pool.imap(processing, events), total=len(events)))
 
             full_signals = [event_signals[0] for event_signals in events_signals]
-            trunc_signals = [event_signals[1] for event_signals in events_signals]
+            frac_signals = [event_signals[1] for event_signals in events_signals]
 
             full_signals = np.array(full_signals)
-            trunc_signals = np.array(trunc_signals)
-            signal_size = trunc_signals.shape[-1]
-            trunc_signals = trunc_signals.reshape(-1, signal_size)
-            _save_detector_signals(full_signals, trunc_signals, detector)
+            frac_signals = np.array(frac_signals)
+            signal_size = frac_signals.shape[-1]
+            frac_signals = frac_signals.reshape(-1, signal_size)
+            _save_detector_signals(full_signals, frac_signals, detector)
 
 
 if __name__ == '__main__':
