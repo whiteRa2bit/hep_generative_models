08d4dd5e5b69d806bbe3a894c71a2561aef6c040
whiteRa2bit <pavel.fakanov@gmail.com>
2020-11-14 17:54:48 +0000

    Add saving of events and detectors

--- tools/data_preparation/prepare_dfs.py
+++ tools/data_preparation/prepare_dfs.py
@@ -2,11 +2,12 @@ import multiprocessing as mp
 import os
 
 import tqdm
+import numpy as np
 from loguru import logger
 
 from generation.utils import timer
 from generation.dataset.data_utils import get_attributes_df, get_event_dir, get_event_detector_df_path, create_dir
-from generation.config import DF_DIR
+from generation.config import DF_DIR, EVENTS_PATH, DETECTORS_PATH
 
 _df_full = get_attributes_df()
 _events = sorted(_df_full["event"].unique())
@@ -31,6 +32,9 @@ def _prepare_event_df(event, df_dir: str = DF_DIR):  # TODO: (@whiteRa2bit, 2020
 @timer
 def main():
     _create_dirs()
+
+    np.save(EVENTS_PATH, _events)
+    np.save(DETECTORS_PATH, _detectors)
 
     logger.info(f'Preparing events dfs...')
     for event in tqdm.tqdm(_events):
