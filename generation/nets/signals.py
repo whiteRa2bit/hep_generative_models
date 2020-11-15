1bb2714a1bc9ec2764b47b75e64f8c40cc05b1ca
whiteRa2bit <pavel.fakanov@gmail.com>
2020-11-15 04:22:53 +0000

    Fc, elu activation, batchnorm

--- generation/nets/signals.py
+++ generation/nets/signals.py
@@ -12,45 +12,31 @@ class Generator(nn.Module):
         self.x_dim = config['x_dim']
         self.z_dim = config['z_dim']
 
-        self.fc1 = nn.Linear(self.z_dim, self.x_dim + 9)
+        self.activation = nn.ELU()
 
-        self.conv1 = nn.Conv1d(1, 16, 3, padding=1)
-        self.conv2 = nn.Conv1d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv1d(32, 16, 3, padding=1)
-        self.conv4 = nn.Conv1d(16, 9, 3, padding=1)
+        self.fc0 = nn.Linear(self.z_dim, self.z_dim * 9)
+        self.fc1 = nn.Linear(self.z_dim, self.x_dim // 16)
+        self.fc2 = nn.Linear(self.x_dim // 16, self.x_dim // 4)
+        self.fc3 = nn.Linear(self.x_dim // 4, self.x_dim)
 
-        self.pool = nn.AvgPool1d(10, stride=1)
-
-        self.batchnorm1 = nn.BatchNorm1d(16)
-        self.batchnorm2 = nn.BatchNorm1d(32)
-        self.batchnorm3 = nn.BatchNorm1d(16)
+        self.batchnorm1 = nn.BatchNorm1d(self.x_dim // 16)
+        self.batchnorm2 = nn.BatchNorm1d(self.x_dim // 4)
 
     def forward(self, x, debug=False):
         def _debug():
             if debug:
-                logger.info(x.shape)
+                print(x.shape)
 
-        x = torch.tanh(self.fc1(x))
+        x = self.activation(self.fc0(x))
         _debug()
-        x = x.unsqueeze(1)
+        x = x.view(-1, 9, self.z_dim)
         _debug()
-        x = self.conv1(x)
-        # x = torch.tanh(x)
-        x = torch.tanh(self.batchnorm1(x))
+        x = self.batchnorm1(self.activation(self.fc1(x)))
         _debug()
-        x = self.conv2(x)
-        # x = torch.tanh(x)
-        x = torch.tanh(self.batchnorm2(x))
+        x = self.batchnorm2(self.activation(self.fc2(x)))
         _debug()
-        x = self.conv3(x)
-        # x = torch.tanh(x)
-        x = torch.tanh(self.batchnorm3(x))
+        x = self.activation(self.fc3(x))
         _debug()
-        x = self.conv4(x)
-        _debug()
-        x = self.pool(x)
-        _debug()
-
         return x
 
     @staticmethod
