6d43e564e7a9191d8136947b09ae34a3f93f8e18
whiteRa2bit <pavel.fakanov@gmail.com>
2020-11-19 16:55:22 +0000

    udpate spectrograms nb

--- generation/config.py
+++ generation/config.py
@@ -38,7 +38,7 @@ SIGNALS_TRAINING_CONFIG = {
     "g_lr": 3e-4,
     "d_lr": 3e-4,
     "epochs_num": 1000,
-    "batch_size": 512,
+    "batch_size": 128,
     "log_each": 1,
     "decay_epoch": 0,
     "save_each": 2,
