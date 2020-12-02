de794b75cffd8e47aeda67bd3c42e00ec41246cf
whiteRa2bit <pavel.fakanov@gmail.com>
2020-12-02 09:58:33 +0000

    bs 32

--- generation/config.py
+++ generation/config.py
@@ -38,7 +38,7 @@ SIGNALS_TRAINING_CONFIG = {
     "g_lr": 3e-4,
     "d_lr": 1e-4,
     "epochs_num": 1000,
-    "batch_size": 16,
+    "batch_size": 32,
     "log_each": 1,
     "decay_epoch": 0,
     "save_each": 2,
