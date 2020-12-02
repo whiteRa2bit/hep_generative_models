3094bf975387ef4720a634a1aa51a4af5920e2e5
whiteRa2bit <pavel.fakanov@gmail.com>
2020-12-02 17:30:53 +0000

    bump wandb

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ requirements = [
     "scipy==1.4.1",
     "numpy==1.18.1",
     "matplotlib==3.2.1",
-    "wandb==0.10.9",
+    "wandb==0.10.11",
     "uproot==3.12.0",
     "tqdm==4.48.2",
-    "h5py==2.10.0"
+    "h5py==2.10.0",
     "loguru==0.5.3",
     "pandas==1.1.0",
     "plotly==4.10.0"
