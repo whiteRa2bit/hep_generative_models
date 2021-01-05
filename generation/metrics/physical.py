c2447e9349849258868f38ce49fe4e5846f0b240
whiteRa2bit <pavel.fakanov@gmail.com>
2021-01-05 08:43:15 +0000

    Update docs

--- generation/metrics/physical.py
+++ generation/metrics/physical.py
@@ -7,7 +7,8 @@ from generation.dataset.data_utils import postprocess_signal
 
 def get_energy_characteristic(signals):
     """
-    Returns space characteristic for a given set of signals
+    Returns space characteristic for a given set of signals.
+    Space characteristic is a distribution of ratios a signal energy to an amplitude
     :param signals: signals np array of shape [detectors_num, signals_num, signal_size]
     :returns: space characteristic
     """
@@ -18,7 +19,7 @@ def get_energy_characteristic(signals):
 
 
 def get_space_characteristic(signals):
-    pass
+    raise ValueError("Not implemented yet")
 
 
 def _get_time_prediction(signal):
@@ -30,6 +31,12 @@ def _get_time_prediction(signal):
 
 
 def get_time_characteristic(signals):
+    """
+    Returns time characteristic for a given set of signals.
+    Time characteristic is a reference time distribution for signals. 
+    :param signals: signals np array of shape [detectors_num, signals_num, signal_size]
+    :returns: space characteristic
+    """
     logger.info("Processing signals for time characteristic")
     postprocessed_signals = [postprocess_signal(signal) for signal in tqdm.tqdm(signals)]
     time_preds = [_get_time_prediction(signal) for signal in postprocessed_signals]
