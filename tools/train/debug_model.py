11ce9ead67b6e5b08aae52461d49a4667d22a27a
whiteRa2bit <pavel.fakanov@gmail.com>
2021-02-10 14:56:35 +0000

    Train into one script

--- tools/train/debug_model.py
+++ tools/train/debug_model.py
@@ -1,18 +1,18 @@
 import torch
 
 from generation.config import SIGNALS_TRAINING_CONFIG as CONFIG
-from generation.dataset.signals_dataset import SignalsDataset
-from generation.nets.signals_net import Generator, Discriminator
+from generation.dataset.shapes_dataset import ShapesDataset
+from generation.nets.shapes_net import ShapesGenerator, ShapesDiscriminator
 from generation.training.wgan_trainer import WganTrainer
 from generation.utils import set_seed
 
 
 def main(config=CONFIG):
     z = torch.randn((config["batch_size"], config["z_dim"]))
-    generator = Generator(config)
-    discriminator = Discriminator(config)
+    generator = ShapesGenerator(config)
+    discriminator = ShapesDiscriminator(config)
     gen_out = generator(z, debug=True)
-    assert gen_out.shape == (config["batch_size"], 9, config["x_dim"])
+    assert gen_out.shape == (config["batch_size"], config["x_dim"])
     preds = discriminator(gen_out, debug=True)
     assert preds.shape == (config["batch_size"], 1)
     print(generator)
---
+++ tools/train/train.py
@@ -0,0 +1,72 @@
+import argparse
+
+import torch
+
+from generation.config import AMPLITUDES_MODEL_NAME, IMAGES_MODEL_NAME, SHAPES_MODEL_NAME, SIGNALS_MODEL_NAME, \
+    AMPLITUDES_TRAINING_CONFIG, IMAGES_TRAINING_CONFIG, SHAPES_TRAINING_CONFIG, SIGNALS_TRAINING_CONFIG
+from generation.dataset.amplitudes_dataset import AmplitudesDataset
+from generation.nets.amplitudes_net import AmplitudesGenerator, AmplitudesDiscriminator
+from generation.dataset.images_dataset import ImagesDataset
+from generation.nets.images_net import ImagesGenerator, ImagesDiscriminator
+from generation.dataset.shapes_dataset import ShapesDataset
+from generation.nets.shapes_net import ShapesGenerator, ShapesDiscriminator
+from generation.dataset.signals_dataset import SignalsDataset
+from generation.nets.signals_net import SignalsGenerator, SignalsDiscriminator
+from generation.training.wgan_trainer import WganTrainer
+from generation.utils import set_seed
+from scheduler import get_gpu_id
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '-m',
+        '--model_name',
+        choices=[AMPLITUDES_MODEL_NAME, IMAGES_MODEL_NAME, SHAPES_MODEL_NAME, SIGNALS_MODEL_NAME],
+        required=True)
+    args = argparser.parse_args()
+    return args
+
+
+def _get_params_by_model_name(model_name):
+    if model_name == AMPLITUDES_MODEL_NAME:
+        config = AMPLITUDES_TRAINING_CONFIG
+        dataset = AmplitudesDataset()
+        generator = AmplitudesGenerator(config)
+        discriminator = AmplitudesDiscriminator(config)
+    elif model_name == IMAGES_MODEL_NAME:
+        config = IMAGES_TRAINING_CONFIG
+        dataset = ImagesDataset(detector=config["detector"])
+        generator = ImagesGenerator(config)
+        discriminator = ImagesDiscriminator(config)
+    elif model_name == SHAPES_MODEL_NAME:
+        config = SHAPES_TRAINING_CONFIG
+        dataset = ShapesDataset(detector=config["detector"], signal_dim=config["x_dim"])
+        generator = ShapesGenerator(config)
+        discriminator = ShapesDiscriminator(config)
+    elif model_name == SIGNALS_MODEL_NAME:
+        config = SIGNALS_TRAINING_CONFIG
+        dataset = SignalsDataset(signal_dim=config["x_dim"], freq=config["x_freq"])
+        generator = SignalsGenerator(config)
+        discriminator = SignalsDiscriminator(config)
+
+    return dataset, generator, discriminator, config
+
+
+def run_train(model_name):
+    dataset, generator, discriminator, config = _get_params_by_model_name(model_name)
+    config["device"] = f"cuda:{get_gpu_id()}"
+
+    g_optimizer = torch.optim.Adam(
+        generator.parameters(), lr=config['g_lr'], betas=(config["g_beta1"], config["g_beta2"]))
+    d_optimizer = torch.optim.Adam(
+        discriminator.parameters(), lr=config['d_lr'], betas=(config["d_beta1"], config["d_beta2"]))
+
+    trainer = WganTrainer(generator, discriminator, g_optimizer, d_optimizer, config)
+    trainer.run_train(dataset)
+
+
+if __name__ == '__main__':
+    set_seed()
+    args = parse_args()
+    run_train(args.model_name)
