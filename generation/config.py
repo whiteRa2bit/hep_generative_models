4d03a38e6c332627a6e213a6035618e16ee4eeb5
whiteRa2bit <pavel.fakanov@gmail.com>
2021-02-10 14:58:27 +0000

    Yapf

--- generation/config.py
+++ generation/config.py
@@ -24,6 +24,12 @@ ATTRIBUTES = ['x', 'y', 'z', 'PhotonEnergy', 'detector', 'timestamp',
 INT_ATTRIBUTES = ['event', 'detector']
 SPACAL_DATA_PATH = os.path.join(RAW_DATA_DIR, 'particles.parquet')
 
+# Models params
+AMPLITUDES_MODEL_NAME = "amplitudes_model"
+IMAGES_MODEL_NAME = "images_model"
+SHAPES_MODEL_NAME = "shapes_model"
+SIGNALS_MODEL_NAME = "signals_model"
+
 # Processing params
 SIGNAL_DIM = 2048
 PROCESSING_TIME_NORM_COEF = 50
@@ -66,16 +72,33 @@ SIGNALS_TRAINING_CONFIG = {
 }
 SHAPES_TRAINING_CONFIG = {
     "detector": 0,
-    "lr": 1e-5,
-    "epochs_num": 5000,
-    "batch_size": 256,
-    "log_each": 1,
+    "g_lr": 3e-4,
+    "d_lr": 1e-4,
+    "epochs_num": 1000,
+    "batch_size": 32,
+    "log_each": 5,
+    "decay_epoch": 0,
     "save_each": 2,
-    "device": "cuda:3",
-    "x_dim": 1024,
-    "z_dim": 8,
-    "disc_coef": 5,
-    "lambda": 5
+    "device": "cuda:1",
+    "x_dim": 128,
+    "x_freq": 2,
+    "z_dim": 64,
+    "d_coef": 5,
+    "use_gp": True,
+    "clip_value": 0.01,
+    "lambda": 10,
+    "channels": 64,
+    "pad_size": 9,
+    "g_use_scheduler": False,
+    "g_lr_multiplier": 5,
+    "g_lr_total_epoch": 200,
+    "d_use_scheduler": False,
+    "d_lr_multiplier": 5,
+    "d_lr_total_epoch": 200,
+    "g_beta1": 0,
+    "g_beta2": 0.99,
+    "d_beta1": 0,
+    "d_beta2": 0.99
 }
 AMPLITUDES_TRAINING_CONFIG = {
     "lr": 1e-5,
