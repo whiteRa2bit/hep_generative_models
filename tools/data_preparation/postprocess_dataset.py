5a88fa32e2fac22e211d9914f606eeefa72d8947
whiteRa2bit <pavel.fakanov@gmail.com>
2021-04-28 18:58:53 +0000

    Conditional + dif energies

--- tools/data_preparation/postprocess_dataset.py
+++ tools/data_preparation/postprocess_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 import multiprocessing as mp
 
 import tqdm
@@ -9,6 +10,7 @@ from generation.dataset.data_utils import postprocess_signal
 from generation.dataset.signals_dataset import SignalsDataset
 
 _NUM_WORKERS = 24
+_SIGNALS_LIMIT = 250
 
 
 def main():
@@ -19,10 +21,11 @@ def main():
     postprocessed_signals = []
     for detector in range(detectors_num):
         logger.info(f"Processing detector {detector}")
+        detector_signals = signals[detector][:_SIGNALS_LIMIT]
         with mp.Pool(_NUM_WORKERS) as pool:
-            postprocessed_detector_signals = list(tqdm.tqdm(pool.imap(postprocess_signal, signals[detector]), total=len(signals[detector])))
+            postprocessed_detector_signals = list(tqdm.tqdm(pool.imap(postprocess_signal, detector_signals), total=len(detector_signals)))
         postprocessed_signals.append(postprocessed_detector_signals)
-        
+
     postprocessed_signals = np.array(postprocessed_signals)
     np.save(POSTPROCESSED_SIGNALS_PATH, postprocessed_signals)
 
--- tools/data_preparation/prepare_signals.py
+++ tools/data_preparation/prepare_signals.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import functools
 import multiprocessing as mp
@@ -13,9 +14,19 @@ from generation.dataset.data_utils import save_h5, get_events, get_detectors, ge
     generate_one_signal, generate_signals, get_detector_signals_path, get_detector_training_data_path, create_dir
 
 _PROCESSORS_NUM = 16
+_EVENTS_LIMIT = 500
 
 
-def _create_dirs(full_signals_dir: str = FULL_SIGNALS_DIR, frac_signals_dir: str = FRAC_SIGNALS_DIR):
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--frac', type=float, required=True)
+    args = argparser.parse_args()
+    return args
+
+
+def _create_dirs(frac, full_signals_dir: str = FULL_SIGNALS_DIR, frac_signals_dir: str = FRAC_SIGNALS_DIR):
+    full_signals_dir = os.path.join(full_signals_dir, str(frac))  # TODO: Remove
+    frac_signals_dir = os.path.join(frac_signals_dir, str(frac))  # TODO: Remove
     create_dir(full_signals_dir)
     create_dir(frac_signals_dir)
 
@@ -27,24 +38,35 @@ def _get_event_detector_signals(event, detector, n_signals=REPEAT_COEF, frac_coe
     return full_signal, frac_signals
 
 
-def _save_detector_signals(full_signals, frac_signals, detector):
+def _save_detector_signals(full_signals, frac_signals, detector, frac):
+    def _modify_path(path):
+        path = path.split('/')
+        path = path[:6] + [str(frac)] + path[6:]
+        return '/'.join(path)
+
     full_data_path = get_detector_signals_path(detector)
     frac_data_path = get_detector_training_data_path(detector)
+    
+    full_data_path = _modify_path(full_data_path)  # TODO: Remove
+    frac_data_path = _modify_path(frac_data_path)  # TODO: Remove
+
     save_h5(full_signals, H5_DATASET_NAME, full_data_path)
     save_h5(frac_signals, H5_DATASET_NAME, frac_data_path)
 
 
 @timer
 def main():
-    _create_dirs()
-
-    events = get_events()
+    args = parse_args()
+    events = get_events()[:_EVENTS_LIMIT]
     detectors = get_detectors()
+
+    _create_dirs(args.frac)
+
 
     with mp.Pool(_PROCESSORS_NUM) as pool:
         for detector in detectors:
             logger.info(f"Processing detector {detector}...")
-            processing = functools.partial(_get_event_detector_signals, detector=detector)
+            processing = functools.partial(_get_event_detector_signals, detector=detector, frac_coef=args.frac)
             events_signals = list(tqdm.tqdm(pool.imap(processing, events), total=len(events)))
 
             full_signals = [event_signals[0] for event_signals in events_signals]
@@ -54,7 +76,7 @@ def main():
             frac_signals = np.array(frac_signals)
             signal_dim = frac_signals.shape[-1]
             frac_signals = frac_signals.reshape(-1, signal_dim)
-            _save_detector_signals(full_signals, frac_signals, detector)
+            _save_detector_signals(full_signals, frac_signals, detector, frac=args.frac)
 
 
 if __name__ == '__main__':
